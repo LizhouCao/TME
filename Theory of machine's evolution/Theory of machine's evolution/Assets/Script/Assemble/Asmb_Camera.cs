e9e71fc46138f2ef48b0f77b2d07dcc3529684bc
LizhouCao <lizhoucao1990@gmail.com>
2017-02-03 16:45:04 -0600

    hahah
    wocalei

---
+++ Theory of machine's evolution/Theory of machine's evolution/Assets/Script/Assemble/Asmb_Camera.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asmb_Camera : MonoBehaviour {
+    public GameObject body;
+    public GameObject sphere;
+
+    private float xl, yl;
+    private int type = 0;
+
+    private float ynum = 0.0f;
+    private float maxDegree = 0.5f;
+    private float minDegree = -1.2f;
+
+    public float hua = 0.0f;
+    private float maxDistance = 2.0f;
+    private float minDistance = -0.4f;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    public void setBody(GameObject obj) {
+        body = obj;
+        transform.LookAt(body.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (body != null && Input.GetMouseButton(1)) {
+            if (type == 0) {
+                type = 1;
+                xl = Input.mousePosition.x;
+                yl = Input.mousePosition.y;
+            }
+            else
+                mRotate();
+        }
+        else {
+            type = 0;
+        }
+        mTransform();
+
+    }
+
+    void mTransform() {
+        float tmp = Input.GetAxis("Mouse ScrollWheel");
+        if (tmp != 0.0f) {
+            if (hua + tmp > minDistance && hua + tmp < maxDistance) {
+                hua += tmp;
+                transform.Translate(new Vector3(0.0f, 0.0f, tmp * 3.0f));
+            }
+        }
+    }
+
+    void mRotate() {
+        float x = Input.mousePosition.x;
+        float y = Input.mousePosition.y;
+        float dx = xl - x;
+        float dy = yl - y;
+        xl = x;
+        yl = y;
+
+        sphere.transform.RotateAround(new Vector3(0.0f, 1.0f, 0.0f), -dx / 100.0f);
+
+        if (ynum + dy / 100.0f >= minDegree && ynum + dy / 100.0f < maxDegree) {
+            ynum += dy / 100.0f;
+            sphere.transform.RotateAround(sphere.transform.right, dy / 100.0f);
+        }
+        transform.LookAt(body.transform.position);
+    }
+}
---
+++ Theory of machine's evolution/Theory of machine's evolution/Assets/Script/Assemble/BoundingBox_Plane.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundingBox_Plane : MonoBehaviour {
+
+    private Color color_Yes = new Color(0.1f, 0.5f, 0.1f, 0.3f);
+    private Color color_No = new Color(0.5f, 0.1f, 0.1f, 0.3f);
+    private Color color_Normal;
+
+    private void OnMouseEnter() {
+        PointerEnter();
+    }
+
+    private void OnMouseExit() {
+        PointerExit();
+    }
+
+    void PointerEnter() {
+        this.GetComponent<Renderer>().material.color = color_Yes;
+    }
+
+    void PointerExit() {
+        this.GetComponent<Renderer>().material.color = color_Normal;
+    }
+
+    // Use this for initialization
+    void Start () {
+        color_Normal = this.GetComponent<Renderer>().material.color;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    
+	}
+}
